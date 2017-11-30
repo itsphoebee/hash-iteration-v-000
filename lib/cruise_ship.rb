022285b34877a9466cbb898135be77dd936b5645
Phoebe Lau <phoebelau7@gmail.com>
2017-11-30 01:54:50 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,12 +8,12 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = " "
+  winner = ""
   passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
-      winner == name
+      winner = name
     end
-end
+  end
 
-winner
+  winner  
 end
